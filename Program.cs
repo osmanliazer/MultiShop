f7d1a8a390ace3c4acac6513cc071f7548d1b437
osmanli azer <azer@osmanli.com>
2024-05-10 06:13:57 +0400

    practic

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace MultiShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute("default", "{controller=home}/{action=index}");
+
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
